8739515b3a0a27c03791d745326edc7937400f35
mattbrownca <6611786+MattBrownCA@users.noreply.github.com>
2020-05-18 22:00:18 -0400

    Working on encoder

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,75 @@
 use std::vec::Vec;
-use std::collections::{LinkedList, HashMap};
+use std::collections::{LinkedList, HashMap, BinaryHeap};
+use std::cmp::Ordering;
 
 extern crate bitstream;
 
-pub struct Huffman {
-    byte_counts: HashMap<u8, usize>,
+pub struct HuffmanEncoder {
+    byte_counts: HashMap<u8, u64>,
     chunks: LinkedList<Vec<u8>>,
 }
 
-struct FreqTree {
-
+struct FreqTreeNode {
+    data: FreqNodeData,
 }
 
-impl Huffman {
-    pub fn new() -> Huffman {
-        Huffman {
+enum FreqNodeData {
+    Composit (FreqTreeComposit),
+    Value (FreqTreeVal),
+}
+
+struct FreqTreeVal {
+    byte_val: u8,
+    occures: u64
+}
+
+struct FreqTreeComposit {
+    occures: u64,
+    left: Box<FreqTreeNode>,
+    right: Box<FreqTreeNode>,
+}
+
+impl FreqTreeNode {
+    pub fn get_weight(&self) -> u64 {
+        use FreqNodeData::*;
+        match self.data {
+            Composit(c) => c.occures,
+            Value(v) => v.occures,
+        }
+    }
+}
+
+impl PartialEq for FreqTreeNode {
+    fn eq(&self, other: &Self) -> bool {
+        self.get_weight() == other.get_weight()
+    }
+}
+
+impl Eq for FreqTreeNode {}
+
+impl PartialOrd for FreqTreeNode {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for FreqTreeNode {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let my_weight = self.get_weight();
+        let other_weight = other.get_weight();
+        if my_weight < other_weight {
+            Ordering::Greater
+        } else if my_weight > other_weight {
+            Ordering::Less
+        } else {
+            Ordering::Equal
+        }
+    }
+}
+
+impl HuffmanEncoder {
+    pub fn new() -> HuffmanEncoder {
+        HuffmanEncoder {
             byte_counts: HashMap::new(),
             chunks: LinkedList::new(),
         }
@@ -33,8 +88,34 @@ impl Huffman {
         self.chunks.push_back(chunk.to_vec());
     }
 
-    pub fn compress(mut self) -> Vec<u8> {
+    pub fn encoder(mut self) -> Vec<u8> {
 
+    }
+
+    fn build_freq_tree(&self) -> Box<FreqTreeNode> {
+        use FreqNodeData::*;
+        let mut heap: BinaryHeap<FreqTreeNode> = BinaryHeap::new();
+
+        // add all of the values to the heap as Value nodes
+        for (k, v) in self.byte_counts {
+            heap.push(FreqTreeNode {data : Value( FreqTreeVal {byte_val: k, occures: v} ) });
+        }
+
+        while heap.len() > 1 {
+            let left = heap.pop().unwrap();
+            let right = heap.pop().unwrap();
+
+            let total = left.get_weight() + right.get_weight();
+
+            let composit = FreqTreeNode { data : Composit( FreqTreeComposit {
+                occures: total,
+                left: Box::new(left),
+                right: Box::new(right),
+            })};
+            heap.push(composit);
+        }
+
+        Box::new(heap.pop().unwrap())
     }
 }
 
